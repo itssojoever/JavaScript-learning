7b1e47dfec96510d885e3d157c0780045d8dc458
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-07-30 20:24:58 +0100

    Commit learning on objects

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // This is a comment
 console.log('Hello world');
+
+//Reassignable
+let name = 'Joe';
+
+//Value of a variable cannot change
+const favouriteNumber = 19;
+
+
+//Object literal
+let person = {
+    name: 'Joe',
+    age: 22
+};
+
+console.log(person)
+
+//Dot notation
+person.name = 'Bob';
+person.age = 28;
+
+console.log(person.name);
+
+//Bracket notation
+person['name'] = 'Jane';
+console.log(person.name);
