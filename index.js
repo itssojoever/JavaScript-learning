3ece2dd11b73a29aae6b857b087bff64c8d9822d
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-08-04 15:07:19 +0100

    commit if statements

--- index.js
+++ index.js
@@ -121,16 +121,27 @@ switch(day){
 console.log(`Today is ${day}`)
 
 //While loop
-running = 5
+let running = 5
 while(running){
     console.log(running)
     running--
 }
 
-running2 = 5
+let running2 = 5
 
 //Do while loop. Evaluates if running2 is True only after having run the loop for one time.
 do{
     console.log(running2)
     running2--
 }while (running2)
+
+
+//If statements
+let age = 23;
+
+if (age >= 18 ){
+    console.log("You are over 18")
+}
+else {
+    console.log("You are not over 18")
+}
